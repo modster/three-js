52703b9905526f1711bed523b2905f2d001d8ea1
E. M. Greeff <893946+modster@users.noreply.github.com>
2022-03-02 03:45:00 -0700

    works

--- js/WebGL.js
+++ js/WebGL.js
@@ -1,3 +1,12 @@
+// if (WebGL.isWebGLAvailable()) {
+// 	// Initiate function or other initializations here
+// 	animate();
+// } else {
+// 	const warning = WebGL.getWebGLErrorMessage();
+// 	document.getElementById('container').appendChild(warning);
+// }
+
+
 class WebGL {
 
 	static isWebGLAvailable() {
